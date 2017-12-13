e985f48f5431c59ab6e9e2a2236bc6e95a85890c
塔洛 <547098912@qq.com>
2017-12-13 19:08:14 +0800

    add basic search

--- app/scripts.babel/popup.js
+++ app/scripts.babel/popup.js
@@ -1,3 +1,34 @@
 'use strict';
 
-console.log('\'Allo \'Allo! Popup');
+// console.log('\'Allo \'Allo! Popup');
+
+var searchBtn = document.getElementById('btn-search');
+var searchInput = document.getElementById('input-search');
+searchBtn.addEventListener('click', function () {
+  var keywords = encodeURIComponent(searchInput.value);
+  google(keywords);
+  bing(keywords);
+  baidu(keywords);
+  yandex(keywords);
+});
+
+function baidu(keywords) {
+  console.log('baidu');
+  var url = `https://www.baidu.com/s?wd=${keywords}`;
+  window.open(url);
+}
+
+function google(keywords) {
+  var url = `https://www.google.com/search?source=hp&q=${keywords}&oq=${keywords}`;
+  window.open(url);
+}
+
+function bing(keywords) {
+  var url = `https://cn.bing.com/search?q=${keywords}&qs=n&form=QBLHCN&sp=-1&pq=${keywords}`;
+  window.open(url);
+}
+
+function yandex(keywords) {
+  var url = `https://www.yandex.com/search/?text=${keywords}`;
+  window.open(url);
+}
